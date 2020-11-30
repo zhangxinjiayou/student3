10e6c4752983a7459b7ea8b121c55a888358f19b
zxx <zxx@qq.com>
2020-11-30 17:09:30 +0800

    在公司开发继续签到功能

---
+++ SignIn.java
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 16:50:18
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:08:50
+*/
+
+public class SignIn {
+    public static void main(String[] args) {
+        private int id ;
+        private String name;
+        public void signIn(){}
+    }
+}
