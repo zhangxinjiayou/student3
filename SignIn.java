2346b1fd40b4d48166616cb52f8c747d065d69e7
email <zhangxin@etoak.comgit config --local user.email zhangxin@etoak.com>
2020-11-30 17:12:45 +0800

    在家开发签到功能其他部分

---
+++ SignIn.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 16:55:49
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:12:09
+*/
+
+public class SignIn {
+    public static void main(String[] args) {
+        private int id;
+        public void sigin(){
+            System.out.println("id"+id);
+
+        }
+    }
+}
